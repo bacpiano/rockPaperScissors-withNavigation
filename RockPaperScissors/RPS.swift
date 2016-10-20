4f83f0de2e606a0affbcc6005b17e9aabd8b2571
bacpiano <borceapiano@yahoo.com>
2016-10-20 10:52:57 -0700

    this is from the “RockPaperScissors-Navigation” section/of the “Meme 2” project/from the iOS Development nano degree/from Udacity.

---
+++ RockPaperScissors/RPS.swift
@@ -0,0 +1,56 @@
+//
+//  RPS
+//  RockPaperScissors
+//
+//  Created by Jason on 11/14/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - RPS: Comparable
+
+enum RPS: Comparable {
+    case Rock, Paper, Scissors
+    
+    init() {
+        switch arc4random() % 3 {
+        case 0:
+            self = .Rock
+        case 1:
+            self = .Paper
+        default:
+            self = .Scissors
+        }
+    }
+}
+
+// MARK: - RPS (Comparison)
+
+func <(lhs: RPS, rhs: RPS) -> Bool {
+    
+    switch (lhs, rhs) {
+    case (.Rock, .Paper), (.Paper, .Scissors), (.Scissors, .Rock):
+        return true
+    default:
+        return false
+    }
+}
+
+// MARK: - RPS: CustomStringConvertible
+
+extension RPS: CustomStringConvertible {
+    
+    var description: String {
+        get {
+            switch (self) {
+            case .Rock:
+                return "Rock"
+            case .Paper:
+                return "Paper"
+            case .Scissors:
+                return "Scissors"
+            }
+        }
+    }
+}
